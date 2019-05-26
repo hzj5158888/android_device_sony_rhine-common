0cf0b12520bb304b857a748d9523937f2e8bf22b
hzj5158888 <www1693303798@163.com>
2019-05-26 09:54:33 +0800

    Revert "shinano-common: Add extract-files and setup-makefiles scripts"
    This reverts commit 75f0de37581f1c8d450791ff0e45b14b56855927.

--- extract-files.sh
+++ extract-files.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
 
 # Use tradition sort
 export LC_ALL=C
@@ -15,26 +14,3 @@ export TARGET_BOARD_PLATFORM=msm8974
 ../common/setup-makefiles.sh
 
 ./setup-makefiles.sh
-=======
-#
-# Copyright (C) 2017 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-set -e
-
-export DEVICE_PLATFORM=msm8974-common
-
-./../../$VENDOR/$DEVICE_PLATFORM/extract-files.sh $@
->>>>>>> 7ac44f1... shinano-common: Add extract-files and setup-makefiles scripts
